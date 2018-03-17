f2d8446aa04719530dbab65ec0d338cc78cb1293
Nyah Check <check.nyah@gmail.com>
2018-03-16 18:40:24 -0700

    Add fixes to data duplication script.

--- server/duplData.js
+++ server/duplData.js
@@ -1,33 +1,47 @@
 var db = require('./db');
 var Promise = require('bluebird');
 var fs = require('fs');
+//const file = fs.createWriteStream('./data.csv');
 
+var newdb = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'yelp_dup'
+});
+
+
+newdb.connect(function(err) {
+  if (err) {
+    console.log('Cant connect to new db');
+  }
+});
 
 var beginSeed = Date.now();
 
 var doubleDataSet = function() {
-  var query = `select * from review`;
-  db.query(query, (err, res) => {
-    if (err) {
-      console.error('Error reading reviews from database');
-      return -1;
-    } else {
-      for (let i = 0, j = 0; i < 10000000 && j < res.length; i++, j++) {
-         if (j === res.length - 1) {
-            j = 0;
-         }
-         var sql = 'INSERT INTO REVIEW (id, business_id, user_id, stars, date, text, useful, funny, cool) values(' + i + ',' + res[i].business_id + ',' + res[i].user_id + ',' + res[i].stars + ',' + res[i].date + ',' + res[i].text + ',' + res[i].useful + ',' + res[i].funny + ',' + res[i].cool + ')';
-         db.query(sql, (err, result) => {
-           if (err) {
-            console.error('Error writing data', err);
-           } else {
-            console.log(i, 'element inserted');
-           }
-         });
+  for (let i = 0; i < 10000000; i += 100) {
+    var query = `select * from review limit 100 offset ${i}`;
+    db.query(query, (err, res) => {
+      if (err) {
+        console.error('Error reading reviews from database');
+        return -1;
+      } else {
+        for (let j = 0; j < res.length; i++) {
+          var sql = 'INSERT INTO REVIEW (id, business_id, user_id, stars, date, text, useful, funny, cool) values(' + i + ',' + res[i].business_id + ',' + res[i].user_id + ',' + res[i].stars + ',' + res[i].date + ',' + res[i].text + ',' + res[i].useful + ',' + res[i].funny + ',' + res[i].cool + ')';
+          console.log('SQL: ', sql);
+          // newdb.query(sql, (err, result) => {
+          //   if (err) {
+          //    console.error('Error writing data', err);
+          //   } else {
+          //    console.log(i, 'element inserted');
+          //   }
+          // });
+        }
       }
-    }
-  });
+    });
+  }
 };
 
 doubleDataSet();
-var bendSeed = Date.now();
+var endSeed = Date.now();
+console.log('Time Diff: ', endSeed, beginSeed);
