6b2f21570edf80c103aa0b5d19adb28a4248e121
Nyah Check <check.nyah@gmail.com>
2018-03-16 20:20:28 -0700

    Got data duplication script to work.

--- server/duplData.js
+++ server/duplData.js
@@ -1,100 +1,80 @@
-var mysql = require('mysql2');
+
 var Promise = require('bluebird');
-var fs = require('fs');
-const Sequelize = require("sequelize");
+var Sequelize = require('sequelize');
+var sequelizeChompy = new Sequelize('yelpData', 'root', '',
+  { host: 'localhost', dialect: 'mysql', logging: false });
+var sequelizeYelp = new Sequelize('yelp_db', 'root', '',
+  { host: 'localhost', dialect: 'mysql', logging: false });
 
-//const file = fs.createWriteStream('./data.csv');
-const db = new Sequelize("yelp_db", "root", "", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-});
+var auth1 = sequelizeChompy
+  .authenticate()
+  .then(() => console.log('Connected to chompy_db'))
+  .catch(err => console.error('Unable to connect to chompy_db', err));
 
-var newdb = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'yelp_dup'
-});
+var auth2 = sequelizeYelp
+  .authenticate()
+  .then(() => console.log('Connected to yelp_db'))
+  .catch(err => console.error('Unable to connect to yelp_db', err));
 
-newdb.connect(function(err) {
-  if (err) {
-    console.log('Cant connect to new db');
+var auths = [auth1, auth2];
+
+var insertBatch = function(limit, offset, counter) {
+  if (offset >= 4174567) {
+    sequelizeYelp.close();
+    sequelizeChompy.close();
+    var endTime = Date.now();
+    console.log('TIME TAKEN:', (endTime - startTime) / 1000);
+    return;
   }
-});
-
-var beginSeed = Date.now();
-
-// var doubleDataSet = function() {
-
-//   var query = `select * from review`;
-//   db.query(query, (err, res) => {
-//     if (err) {
-//       console.error('Error reading reviews from database');
-//       return -1;
-//     } else {
-//       console.log(res.length);
-//       // for (let i = 0, j = 0; i < 10000000 && j < res.length; i++, j++) {
-//       //    if (j === res.length - 1) {
-//       //       j = 0;
-//       //    }
-//       //    var sql = 'INSERT INTO REVIEW (id, business_id, user_id, stars, date, text, useful, funny, cool) values(' + i + ',' + res[i].business_id + ',' + res[i].user_id + ',' + res[i].stars + ',' + res[i].date + ',' + res[i].text + ',' + res[i].useful + ',' + res[i].funny + ',' + res[i].cool + ')';
-//       //    db.query(sql, (err, result) => {
-//       //      if (err) {
-//       //       console.error('Error writing data', err);
-//       //      } else {
-//       //       console.log(i, 'element inserted');
-//       //      }
-//       //    });
-//       // }
-//     }
-//   });
-// };
-
-// doubleDataSet();
-console.log('Begin: ', beginSeed);
-
-
-
-
-db
-  .query("SELECT * FROM review")
-  .then(data => {
-    var temp = [];
-    for (var i = 0; i < 60; i++) {
-      temp.push(data[0]);
-    }
-    var review = temp.reduce((a, b) => {
-      return a.concat(b);
-    });
-    /*-----------ABOVE DUPES DATA----------------------*/
-    const file = fs.createWriteStream("./data.csv");
-    let write10MnTimes = (n = 1e7) => {
-      let isReady = true;
-      while (n > 0 && isReady) {
-        review[n].id = n;
-        isReady = file.write(
-          `${review[n].id}, ${review[n].business_id}, ${
-            review[n].user_id
-          }, ${review[n].stars}, ${review[n].date}, ${
-            review[n].text
-          }, ${review[n].useful}, ${review[n].funny}, ${
-            review[n].cool
-          } \n`
+  sequelizeYelp.query(`SELECT * FROM review LIMIT ${limit} OFFSET ${offset};`)
+    .then(result => {
+    var reviews = result[0];
+    var insertionPromises = [];
+    for (var i = 0; i < reviews.length; i++) {
+      var queryString = 'INSERT INTO business ' +
+        '(id, business_id, user_id, date, text, useful, funny, cool) ' +
+        'VALUES ';
+      var replacements = [];
+      for (var j = 0; j < 59; j++) {
+        queryString += '(?, ?, ?, ?, ?, ?, ? , ?, ?), ';
+        replacements.push(
+          counter,
+          reviews[i].business_id,
+          reviews[i].user_id,
+          reviews[i].date,
+          reviews[i].text,
+          reviews[i].useful,
+          reviews[i].funny,
+          reviews[i].cool
         );
-        n--;
+        counter++;
       }
-      file.once("drain", () => {
-        write10MnTimes(n);
-        console.log("draining at ", n);
-      });
-    };
-    write10MnTimes();
+      queryString += '(?, ?, ?, ?, ?, ?);';
+      replacements.push(
+        counter,
+        reviews[i].business_id,
+        reviews[i].user_id,
+        reviews[i].date,
+        reviews[i].text,
+        reviews[i].useful,
+        reviews[i].funny,
+        reviews[i].cool
+      );
+      counter++;
+
+      insertionPromises.push(
+        sequelizeChompy.query(queryString, { replacements: replacements })
+      );
+
+    }
+    return Promise.all(insertionPromises);
+  }).catch((err) => {
+    console.error('Failed to seed Chompy database', err);
+  }).then(() => {
+    console.log(`Successfully inserted ${limit} * 60 items into Chompy database. Offset: ${offset}`);
+    return insertBatch(limit, offset + limit, counter);
   })
-  .then(data => {
-    console.log("review done");
-    var endSeed = Date.now();
-    console.log('Begin: ', endSeed);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+}
+
+var startTime = Date.now()
+Promise.all(auths).then(() => insertBatch(1000, 0, 1));
